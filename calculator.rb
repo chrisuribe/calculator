1a463cd6ca82d60cb8248263f350fa02d52b9185
Chris Uribe <chris.uribe.is@gmail.com>
2019-06-10 13:31:06 -0700

    First example of code written with students.

---
+++ calculator.rb
@@ -0,0 +1,62 @@
+class Calculator
+    def one_calculation()
+      puts "Ruby Calculator"
+      puts "What would you like to do?"
+      puts "1. Add"
+      puts "2. Subtract"
+      puts "3. Multiply"
+      puts "4. Divide"
+      puts "Pick an option above (1-4)"
+
+      menu_option = gets.chomp().to_i
+
+      puts "Please enter the first of two numbers which you would like to "
+
+      if menu_option == 1
+        puts "add."
+      elsif menu_option == 2
+        puts "subtract."
+      elsif menu_option == 3
+        puts "multiply."
+      elsif menu_option == 4
+        puts "divide."
+      else
+        puts "Invalid option!!!!! :( Try again.\n\n"
+        return
+      end
+
+      
+
+      number_1 = gets.chomp().to_f;
+
+      puts "Your first number is #{number_1}."
+      puts "Please enter the second number."
+
+      number_2 = gets.chomp().to_f;
+
+      puts "The answer is: "
+
+      if menu_option == 1
+        puts (number_1 + number_2)
+      elsif menu_option == 2
+        puts (number_1 - number_2)
+      elsif menu_option == 3
+        puts (number_1 * number_2)
+      elsif menu_option == 4
+        puts (number_1 / number_2)
+      else
+        puts "invalid input. :( Try again."
+      end
+    end
+
+    def version1
+      is_running = true;
+
+      while is_running do 
+        menu
+      end
+    end
+  end
+  
+  s = Calculator.new
+  s.one_calculation
